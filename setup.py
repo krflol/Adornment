112568d75e67111cef5977f4b203d5e75f36e49e
krflol <keenanfinkelstein@gmail.com>
2023-12-10 13:04:29 -0600

    renaming

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="llm_debug",
+    name="adornment",
     version="0.1",
     packages=find_packages(),
     author="Keenan Finkelstein",
     author_email="keenanfinkelstein@gmail.com",
-    description="A decorator for debugging functions in Python.",
+    description="A collection of llm decorators.",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url="http://github.com/yourusername/my_package",
